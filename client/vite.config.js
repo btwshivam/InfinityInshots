2a190cf7a3583cce9ba3b8c65a4d2162daef98d2
Shivam Kumar <127589548+btwshivam@users.noreply.github.com>
2024-05-13 00:20:26 +0530

    Update vite.config.js for backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
+        target: 'https://infinityinshots.onrender.com',
+        secure: true,
       },
     },
   },
